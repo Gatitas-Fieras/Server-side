bef382343f4f634cb43dd37524494a8c85ee20ff
SebTou08 <sebaas14toulier@gmail.com>
2021-10-01 17:48:11 -0500

    refactor: change API_URL

--- src/test/java/com/example/ilenguageapi/cucumber/seleniumConfig/SeleniumWebDriverConfig.java
+++ src/test/java/com/example/ilenguageapi/cucumber/seleniumConfig/SeleniumWebDriverConfig.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 
 public class SeleniumWebDriverConfig {
     public WebDriver driver;
-    public  String API_URL = "https://homehealthy.netlify.app";
+    public  String API_URL = "http://localhost:4200/home";
     public static String PATH_DRIVER = System.getProperty("user.dir") + "\\driver\\windows\\chromedriver.exe";
 
 }
