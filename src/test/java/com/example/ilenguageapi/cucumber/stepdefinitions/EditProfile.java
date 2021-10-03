c73513007fb2b9b14c9578a6f77bf4cc97fa67d2
SebTou08 <sebaas14toulier@gmail.com>
2021-10-03 14:11:11 -0500

    feat: add step definition for editing profile

---
+++ src/test/java/com/example/ilenguageapi/cucumber/stepdefinitions/EditProfile.java
@@ -0,0 +1,39 @@
+package com.example.ilenguageapi.cucumber.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EditProfile {
+    @Given("que el profesor esta iniciado sesión en la aplicación")
+    public void queElProfesorEstaIniciadoSesiónEnLaAplicación() {
+    }
+
+    @When("le dé clic a la sección de mi perfil")
+    public void leDéClicALaSecciónDeMiPerfil() {
+    }
+
+    @Then("el sistema le muestra el perfil de usuario con información como foto de perfil, datos personales, experiencia \\(por el sistema)")
+    public void elSistemaLeMuestraElPerfilDeUsuarioConInformaciónComoFotoDePerfilDatosPersonalesExperienciaPorElSistema() {
+    }
+
+    @Given("que el profesor se encuentra en la sección de perfil")
+    public void queElProfesorSeEncuentraEnLaSecciónDePerfil() {
+    }
+
+    @When("cuando le da clic a la opción de editar mi perfil")
+    public void cuandoLeDaClicALaOpciónDeEditarMiPerfil() {
+    }
+
+    @Then("el sistema le permite editar los campos como datos personales")
+    public void elSistemaLePermiteEditarLosCamposComoDatosPersonales() {
+    }
+
+    @When("cuando le da clic a la opción de editar mi perfil e ingrese datos erroneos")
+    public void cuandoLeDaClicALaOpciónDeEditarMiPerfilEIngreseDatosErroneos() {
+    }
+
+    @Then("el sistema no podrá realizar la modificacion")
+    public void elSistemaNoPodráRealizarLaModificacion() {
+    }
+}
