61855a27bfb0272760727d2bd7b6c748e41f22e1
SebTou08 <sebaas14toulier@gmail.com>
2021-10-03 14:17:37 -0500

    feat: compleate first scenario

--- src/test/java/com/example/ilenguageapi/cucumber/stepdefinitions/EditProfile.java
+++ src/test/java/com/example/ilenguageapi/cucumber/stepdefinitions/EditProfile.java
@@ -1,24 +1,43 @@
 package com.example.ilenguageapi.cucumber.stepdefinitions;
 
+import com.example.ilenguageapi.cucumber.seleniumConfig.SeleniumWebDriverConfig;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+
+import static java.lang.Thread.sleep;
 
 public class EditProfile {
+
+    SeleniumWebDriverConfig myDriver;
+
+    public EditProfile() {
+        myDriver = new SeleniumWebDriverConfig();
+    }
+
     @Given("que el profesor esta iniciado sesión en la aplicación")
-    public void queElProfesorEstaIniciadoSesiónEnLaAplicación() {
+    public void queElProfesorEstaIniciadoSesiónEnLaAplicación() throws InterruptedException {
+        myDriver.logInAsTutor();
+
     }
 
     @When("le dé clic a la sección de mi perfil")
     public void leDéClicALaSecciónDeMiPerfil() {
+        myDriver.driver.findElement(By.xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-session/mat-drawer-container/mat-drawer/div/button[2]")).click();
     }
 
     @Then("el sistema le muestra el perfil de usuario con información como foto de perfil, datos personales, experiencia \\(por el sistema)")
-    public void elSistemaLeMuestraElPerfilDeUsuarioConInformaciónComoFotoDePerfilDatosPersonalesExperienciaPorElSistema() {
+    public void elSistemaLeMuestraElPerfilDeUsuarioConInformaciónComoFotoDePerfilDatosPersonalesExperienciaPorElSistema() throws InterruptedException {
+        sleep(2000);
+        myDriver.closeWindow();
     }
 
     @Given("que el profesor se encuentra en la sección de perfil")
-    public void queElProfesorSeEncuentraEnLaSecciónDePerfil() {
+    public void queElProfesorSeEncuentraEnLaSecciónDePerfil() throws InterruptedException {
+        myDriver.logInAsTutor();
+        sleep(2000);
+        myDriver.driver.findElement(By.xpath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-session/mat-drawer-container/mat-drawer/div/button[2]")).click();
     }
 
     @When("cuando le da clic a la opción de editar mi perfil")
